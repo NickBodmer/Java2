5c95d13c919141081b5109658c55c5f077381357
NickBodmer <bodmerr@yandex.ru>
2020-04-07 16:20:11 +0300

    Initial commit

---
+++ src/Lesson2/Main.java
@@ -0,0 +1,13 @@
+package Lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+        String[][] arr = new String[][]{{"1", "2", "3", "4"}, {"2", "2", "2", "3"},
+                {"1", "2", "2", "2"}, {"2", "2", "2", "2"}};
+}catch (MyArraySizeExpion)
+
+
+    }
+
+}
